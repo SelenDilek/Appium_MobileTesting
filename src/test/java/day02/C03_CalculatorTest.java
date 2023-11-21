b4f1aa8b322a67b1afcaf9123c01cb1bc0135952
Selen Dilek <yildirimselen17@gmail.com>
2023-11-21 13:59:12 -0500

    OOP Concept

--- src/test/java/day02/C03_CalculatorTest.java
+++ src/test/java/day02/C03_CalculatorTest.java
@@ -1,6 +1,17 @@
 package day02;
 
+import org.testng.annotations.BeforeTest;
+
 public class C03_CalculatorTest {
+    //Appium Inspector
+    // com.google.android.calculator:id/digit_8
+
+    @BeforeTest
+    public void setUpCalculatorApk(){
+
+
+
+    }
 
 
 }
